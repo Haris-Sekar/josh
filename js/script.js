24f9bc1a2b646b6baf67f40c0b4dc0c002ba6146
Haris Sekar <sharis14003@gmail.com>
2022-05-19 23:31:40 +0530

    plan and global part added

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const pricing = document.getElementById('plan');
+const pricingBtn = document.getElementById('pricingBtn');
+pricing.addEventListener("mouseover",(event)=>{
+    pricingBtn.style.backgroundColor = "#F53838";
+    pricingBtn.style.color="#ffffff"
+})
+
+pricing.addEventListener("mouseleave",(event)=>{
+    pricingBtn.style.backgroundColor = "#ffffff";
+    pricingBtn.style.color="#F53838"
+})
+const pricing1 = document.getElementById('plan1');
+const pricingBtn1 = document.getElementById('pricingBtn1');
+pricing1.addEventListener("mouseover",(event)=>{
+    pricingBtn1.style.backgroundColor = "#F53838";
+    pricingBtn1.style.color="#ffffff"
+})
+
+pricing1.addEventListener("mouseleave",(event)=>{
+    pricingBtn1.style.backgroundColor = "#ffffff";
+    pricingBtn1.style.color="#F53838"
+})
+const pricing2 = document.getElementById('plan2');
+const pricingBtn2 = document.getElementById('pricingBtn2');
+pricing2.addEventListener("mouseover",(event)=>{
+    pricingBtn2.style.backgroundColor = "#F53838";
+    pricingBtn2.style.color="#ffffff"
+})
+
+pricing2.addEventListener("mouseleave",(event)=>{
+    pricingBtn2.style.backgroundColor = "#ffffff";
+    pricingBtn2.style.color="#F53838"
+})
