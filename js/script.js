256869c172170372c2b85141ca18c7aa149779b8
Haris Sekar <sharis14003@gmail.com>
2022-05-20 21:48:14 +0530

    website v1 completed

--- js/script.js
+++ js/script.js
@@ -31,3 +31,44 @@ pricing2.addEventListener("mouseleave",(event)=>{
     pricingBtn2.style.backgroundColor = "#ffffff";
     pricingBtn2.style.color="#F53838"
 })
+
+
+
+const review = document.getElementById('review');
+
+review.addEventListener("mouseover",(event)=>{
+    review.style.borderColor = '#F53838';
+    review.style.boxShadow = "2px 50px 70px 0px #cac0c0";
+});
+review.addEventListener("mouseleave",(event)=>{
+    review.style.borderColor = "#DDDDDD";
+    review.style.boxShadow = "none";
+
+})
+
+const review1 = document.getElementById('review1');
+
+review1.addEventListener("mouseover",(event)=>{
+    review1.style.borderColor = '#F53838';
+    review1.style.boxShadow = "2px 50px 70px 0px #cac0c0";
+
+});
+review1.addEventListener("mouseleave",(event)=>{
+    review1.style.borderColor = "#DDDDDD";
+    review1.style.boxShadow = "none";
+
+})
+
+const review2 = document.getElementById('review2');
+
+review2.addEventListener("mouseover",(event)=>{
+    review2.style.borderColor = '#F53838';
+    review2.style.boxShadow = "2px 50px 70px 0px #cac0c0";
+
+});
+review2.addEventListener("mouseleave",(event)=>{
+    review2.style.borderColor = "#DDDDDD";
+    review2.style.boxShadow = "none";
+
+})
+console.log(document.getElementById('plan2').clientHeight);
